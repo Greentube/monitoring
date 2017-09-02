7fb02d35aa6006353903a6035588bee4b21e441f
Bruno Garcia <bruno@brunogarcia.com>
2017-09-02 20:42:18 +0200

    Removed csproj attributes added by dotnet migrate and redundant AssemblyInfo.cs

--- src/Greentube.Monitoring.Redis/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyProduct("Greentube.Monitoring.Redis")]
-
-[assembly: Guid("78a6706f-599a-4cbd-b867-c6fe053788d3")]
