7636e431ad1f01a6fe7acb6897a39ebd856ffb96
mhota <mhotak@greentube.com>
2019-01-16 11:04:55 +0100

    SqlDbMonitoringOptionsExtensions tests for raising code coverage

--- test/Greentube.Monitoring.SqlDb.Tests/SqlDbMonitoringOptionsExtensionsTests.cs
+++ test/Greentube.Monitoring.SqlDb.Tests/SqlDbMonitoringOptionsExtensionsTests.cs
@@ -1,4 +1,8 @@
-﻿using Xunit;
+﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
 
 namespace Greentube.Monitoring.SqlDb.Tests
 {
@@ -11,6 +15,60 @@ namespace Greentube.Monitoring.SqlDb.Tests
         {
             var resourceName = SqlDbMonitoringOptionsExtensions.DbSchemaResourceName(dbConnectionString, schema);
             Assert.Equal(expectedResourceName, resourceName);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void AddSqlDbStoredProcedureMonitor_Ok(bool isCritical)
+        {
+            // ARRANGE
+            var options = new MonitoringOptions();
+            var dbConnectionProvider = Substitute.For<IDbConnectionProvider>();
+            var configuration = Substitute.For<IResourceMonitorConfiguration>();
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            var logger = Substitute.For<ILogger<SqlDbStoredProcedureMonitor>>();
+            serviceProvider.GetService(typeof(ILogger<SqlDbStoredProcedureMonitor>)).Returns(logger);
+
+            // ACT
+            options.AddSqlDbStoredProcedureMonitor(dbConnectionProvider, "schema", "storedProcedureName", "resource", isCritical, configuration);
+
+            // ASSERT
+            var factories = options.Factories.ToList();
+            Assert.Single(factories);
+            var factory = factories.Single();
+            var monitor = factory(configuration, serviceProvider);
+            Assert.IsType<SqlDbStoredProcedureMonitor>(monitor);
+            var sqlMonitor = (SqlDbStoredProcedureMonitor)monitor;
+            Assert.Equal("resource", sqlMonitor.ResourceName);
+            Assert.Equal(configuration, sqlMonitor.Configuration);
+            Assert.Equal(isCritical, sqlMonitor.IsCritical);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void AddSqlDbStoredProcedureMonitor2_Ok(bool isCritical)
+        {
+            // ARRANGE
+            var options = new MonitoringOptions();
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            var logger = Substitute.For<ILogger<SqlDbStoredProcedureMonitor>>();
+            serviceProvider.GetService(typeof(ILogger<SqlDbStoredProcedureMonitor>)).Returns(logger);
+
+            // ACT
+            options.AddSqlDbStoredProcedureMonitor("Application Name=Bar;Data Source=dev-db;Initial Catalog=db_name;UID=baz_user;PWD=123456", "schema", "storedProcedureName", isCritical: isCritical);
+
+            // ASSERT
+            var factories = options.Factories.ToList();
+            Assert.Single(factories);
+            var factory = factories.Single();
+            var monitor = factory(options, serviceProvider);
+            Assert.IsType<SqlDbStoredProcedureMonitor>(monitor);
+            var sqlMonitor = (SqlDbStoredProcedureMonitor)monitor;
+            Assert.Equal("db_name.schema", sqlMonitor.ResourceName);
+            Assert.Equal(options, sqlMonitor.Configuration);
+            Assert.Equal(isCritical, sqlMonitor.IsCritical);
         }
     }
 }
